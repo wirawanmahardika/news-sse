d22445b7b6b648ccff8866aab9de94002d96875a
wirawan-mahardika <wirawanmahardika10@gmail.com>
2023-11-30 14:26:58 +0800

    fix: toggle untuk menampilkan navbar tidak berfungsi

--- public/js/layouts.js
+++ public/js/layouts.js
@@ -1,9 +1,8 @@
-const navbar = document.getElementById("navbar");
+const navbar = document.querySelector(".navbar");
 const navbarBtns = document.getElementsByClassName("navbar-btn");
 
 Array.from(navbarBtns).forEach((btn) => {
-  btn.addEventListener("click", (e) => {
+  btn.addEventListener("click", () => {
     navbar.classList.toggle("navbar-left-0");
   });
 });
-
