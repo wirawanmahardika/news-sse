070ac6d2a9a317ab4ce49d7da75003b46d602340
wirawan-mahardika <wirawanmahardika10@gmail.com>
2023-12-01 01:02:50 +0800

    temp: mengubah mekanisme form add-news

---
+++ public/js/add-news-test.js
@@ -0,0 +1,113 @@
+const contentNumber = document.querySelector(".content-number");
+const jumlahSubJudulContainer = document.querySelector(".jumlah-sub-judul");
+const subJudulContainer = document.getElementById("sub-judul-container");
+const contentContainer = document.getElementById("content-container");
+const form = document.getElementsByTagName("form")[0];
+
+let dataToFetch = [];
+let selectedContent;
+
+jumlahSubJudulContainer.addEventListener("input", (e) => {
+  let jmlSubjudul = e.target.value;
+  contentNumber.innerHTML = "";
+
+  //   const subJudulContainers = document.getElementsByClassName(
+  //     "sub-judul-container"
+  //   );
+  while (true) {
+    const element = document.querySelector(".input-content-container");
+    if (element) {
+      element.remove();
+    } else {
+      break;
+    }
+  }
+
+  for (let i = jmlSubjudul; i > 0; i--) {
+    let inputContentContainer = document.createElement("div");
+    inputContentContainer.classList.add(
+      "w-full",
+      "p-3",
+      "gap-y-2",
+      "flex-col",
+      "input-content-container"
+      //   "hidden"
+    );
+
+    const labelInput = document.createElement("label");
+    labelInput.classList.add("font-bold", "text-lg", "md:text-xl");
+    labelInput.textContent = "Sub Judul Ke " + i;
+
+    const newInput = document.createElement("input");
+    newInput.name = "sub_title" + i;
+    newInput.type = "text";
+    newInput.required = true;
+    newInput.classList.add(
+      "sub-judul-container",
+      "w-full",
+      "px-2",
+      "py-1",
+      "outline-none",
+      "border-2",
+      "border-gray-700",
+      "focus-within:border-cyan-600",
+      "md:py-2",
+      "md:px-3",
+      "lg:py-1"
+    );
+
+    const labelTxtArea = document.createElement("label");
+    labelTxtArea.classList.add("font-bold", "text-lg", "md:text-xl");
+    labelTxtArea.textContent = "Content Ke " + i;
+
+    const newtxtArea = document.createElement("textarea");
+    newtxtArea.name = "content" + i;
+    newtxtArea.cols = "30";
+    newtxtArea.rows = "10";
+    newtxtArea.required = true;
+    newtxtArea.classList.add(
+      "content-container",
+      "w-full",
+      "px-2",
+      "py-1",
+      "outline-none",
+      "border-2",
+      "border-gray-700",
+      "focus-within:border-cyan-600",
+      "md:py-2",
+      "md:px-3",
+      "lg:py-1"
+    );
+
+    e.target.parentElement.after(
+      inputContentContainer,
+      inputContentContainer.cloneNode(true)
+    );
+    e.target.parentElement.nextElementSibling.append(labelInput, newInput);
+    e.target.parentElement.nextElementSibling.nextElementSibling.append(
+      labelTxtArea,
+      newtxtArea
+    );
+
+    // const a = document.createElement("a");
+    // a.dataset.number = i.toString();
+    // a.classList.add(
+    //   "bg-gray-700",
+    //   "py-1",
+    //   "px-3",
+    //   "text-white",
+    //   "hover:bg-cyan-600",
+    //   "cursor-pointer"
+    // );
+    // a.textContent = i;
+    // contentNumber.append(a);
+
+    // if (jmlSubjudul > 0) {
+    //   contentContainer.classList.replace("hidden", "flex");
+    //   contentContainer.firstChild.textContent = "Content Ke-" + 1;
+    //   subJudulContainer.classList.replace("hidden", "flex");
+    //   subJudulContainer.firstChild.textContent = "Subjudul Ke-" + 1;
+    //   selectedContent = 1;
+    // }
+  }
+});
