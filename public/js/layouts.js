48e857a5b38df2f0c34ba3d6f199bec386071dee
wirawanmahardika <wiraprogrammer@gmail.com>
2024-07-21 04:10:48 +0800

    style: mengubah tampilan navbar

--- public/js/layouts.js
+++ public/js/layouts.js
@@ -1,11 +1,11 @@
 const navbar = document.querySelector(".navbar");
 const navbarBtns = document.getElementsByClassName("navbar-btn");
 const searhForm = document.getElementById("search-form")
-const navbarKategori = document.getElementById("navbar-kategori")
+// const navbarKategori = document.getElementById("navbar-kategori")
 
 Array.from(navbarBtns).forEach((btn) => {
   btn.addEventListener("click", () => {
-    if (window.scrollY < 193) window.scrollTo({ top: 193, behavior: "smooth" })
+    if (window.scrollY < 193 && window.innerWidth < 640) window.scrollTo({ top: 193, behavior: "smooth" })
     navbar.classList.toggle("navbar-visibility");
     document.body.classList.toggle("bg-gray-400")
   });
@@ -18,18 +18,21 @@ searhForm.addEventListener("submit", async (e) => {
   window.location.href = '/search-news?judul=' + input
 });
 
+const navbarKategori = document.getElementsByClassName("navbar-kategori");
 (async () => {
   const res = await fetch("/api/v1/category-news")
   const data = await res.json()
 
-  data.map(d => {
-    const li = document.createElement("li")
-    const a = document.createElement("a")
-    a.href = "/"
-    a.textContent = d.category
-    a.classList.add("hover:text-sky-600")
+  Array.from(navbarKategori).forEach(n => {
+    data.map(d => {
+      const li = document.createElement("li")
+      const a = document.createElement("a")
+      a.href = "/"
+      a.textContent = d.category
+      a.classList.add("hover:text-sky-600")
 
-    li.appendChild(a)
-    navbarKategori.prepend(li)
+      li.appendChild(a)
+      n.appendChild(li)
+    })
   })
 })();
